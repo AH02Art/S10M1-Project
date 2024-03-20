de8c9430a1df1863d6bd6265d021572585cda407
Alex Handley <smithvillescholarsah@gmail.com>
2024-03-20 15:48:17 -0500

    S10M1-Project completed

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,28 +1,43 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React, { useReducer } from "react";
 
-// 👇 these are the types of actions that can change state
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
-// 👇 create your initial state object here
+const initialState = {
+  authorName: "",
+  quoteText: ""
+};
 
-// 👇 create your reducer function here
+function reducer(state, action) {
+  switch (action.type) {
+    case CHANGE_INPUT:
+      const { name, value } = action.payload;
+      return { ...state, [name]: value };
+    case RESET_FORM:
+      return { authorName: "", quoteText: "" };
+    default: {
+      return state;
+    };
+  };
+};
 
-export default function TodoForm({ createQuote = () => { } }) {
-  // 👇 use the reducer hook to spin up state and dispatch
+export default function TodoForm({ createQuote }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const onChange = () => {
-    // 👇 implement
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    dispatch({ type: CHANGE_INPUT, payload: { name, value } })
   }
   const resetForm = () => {
-    // 👇 implement
+    dispatch({ type: RESET_FORM });
   }
-  const onNewQuote = () => {
-    // 👇 implement
+  const onNewQuote = (event) => {
+    event.preventDefault();
+    const { authorName, quoteText } = state;
+    createQuote({ authorName, quoteText })
     resetForm()
   }
 
-  // 👇 some props are missing in the JSX below:
   return (
     <form id="quoteForm" onSubmit={onNewQuote}>
       <h3>New Quote Form</h3>
@@ -31,6 +46,7 @@ export default function TodoForm({ createQuote = () => { } }) {
           type='text'
           name='authorName'
           placeholder='type author name'
+          value={state.authorName}
           onChange={onChange}
         />
       </label>
@@ -39,6 +55,7 @@ export default function TodoForm({ createQuote = () => { } }) {
           type='text'
           name='quoteText'
           placeholder='type quote'
+          value={state.quoteText}
           onChange={onChange}
         />
       </label>
